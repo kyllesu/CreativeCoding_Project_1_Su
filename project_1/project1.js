b3b6195f022fdddbfa6f4889696b51e8c1e350ab
kyllesu <68802955+kyllesu@users.noreply.github.com>
2020-10-23 18:59:22 -0700

    tenth push
    WALKING!

--- project_1/project1.js
+++ project_1/project1.js
@@ -6,6 +6,10 @@ let hasStopped = false;
 let heartX, heartY;
 let gradient = 100;
 
+let len = 65;
+let a1, a2, a3, a4;
+let legSpeed = 0;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -35,15 +39,15 @@ function draw() {
 
   person1.displayHead();
   person1.displayBody();
-  person1.displayFrontLeg(1);
-  person1.displayBackLeg(1);
+  person1.displayLegs(1);
+
   person1.move(1);
   person1.stop();
 
   person2.displayHead();
   person2.displayBody();
-  person2.displayFrontLeg(-1);
-  person2.displayBackLeg(-1);
+  person2.displayLegs(-1);
+
   person2.move(-1);
   person2.stop();
 
@@ -160,7 +164,6 @@ class Person {
 
     this.x = this.x + 0.65 * direction; //speed
     this.y = this.y;
-    this.state = (this.state + 5) % 3; // transition between both states to create walking animation
   }
 
   stop() { //will stop the people
@@ -190,24 +193,38 @@ class Person {
     line(this.x, this.y + 5, this.x, this.y + 130); // body
   }
 
-  displayFrontLeg(facing) {
+ displayLegs(facing) {
+    a1 = sin(legSpeed);
+    a2 = sin(legSpeed - 0.4) - 0.3;
+    a3 = sin(PI + legSpeed);
+    a4 = sin(PI + legSpeed - 0.4) - 0.3;
+
+    let leftKneeX = this.x + sin(a1) * len * facing;
+    let leftKneeY = this.y + cos(a1) * len;
+    let rightKneeX = this.x + sin(a3) * len * facing;
+    let rightKneeY = this.y + cos(a3) * len;
+
+    let leftAnkleX = leftKneeX + sin(a2) * len * facing;
+    let leftAnkleY = leftKneeY + cos(a2) * len;
+    let rightAnkleX = rightKneeX + sin(a4) * len * facing;
+    let rightAnkleY = rightKneeY + cos(a4) * len;
+
     if (this.state == 1) {
-      line(this.x, this.y + 130, this.x + 30 * facing, this.y + 170); // forward thigh
-      line(this.x + 30 * facing, this.y + 170, this.x + 10 * facing, this.y + 240); // forward shin
+      line(this.x, this.y + 130, leftKneeX, leftKneeY + 130);
+      line(leftKneeX, leftKneeY + 130, leftAnkleX, leftAnkleY + 130);
+      line(this.x, this.y + 130, rightKneeX, rightKneeY + 130);
+      line(rightKneeX, rightKneeY + 130, rightAnkleX, rightAnkleY + 130);
+
     } else if (this.state == 2) {
-      line(this.x, this.y + 130, this.x + 10 * facing, this.y + 170); //transition
-      line(this.x + 10 * facing, this.y + 170, this.x, this.y + 240);
+        line(this.x, this.y + 130, this.x + 10 * facing, this.y + 170); //transition
+        line(this.x + 10 * facing, this.y + 170, this.x, this.y + 240);
+        line(this.x, this.y + 130, this.x, this.y + 240);
     }
+    legSpeed += 0.02;
+    
   }
 
-  displayBackLeg(facing) {
-    if (this.state == 1) {
-      line(this.x, this.y + 130, this.x - 30 * facing, this.y + 240); // back leg
-    } else if (this.state == 2) {
-      line(this.x, this.y + 130, this.x, this.y + 240); //transition
-    }
 
-  }
 
 }
 //i made a change
